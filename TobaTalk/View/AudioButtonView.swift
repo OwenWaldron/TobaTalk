73dd0c8bf309230bcd2d5de0a95174426589489e
Owen Waldron <owaldron@tfs.ca>
2022-01-30 17:34:47 -0500

    Added tuple notation

--- TobaTalk/View/AudioButtonView.swift
+++ TobaTalk/View/AudioButtonView.swift
@@ -7,21 +7,28 @@
 
 import SwiftUI
 
-struct AudioButtonView: View {
+struct ABView: View {
     let content: String
+    var image = "photo"
     
     var body: some View {
         ZStack {
             let shape = RoundedRectangle(cornerRadius: 20)
             shape.fill().foregroundColor(.white)
             shape.stroke(lineWidth: 3)
-            Text(content).font(.largeTitle)
+            VStack{
+                Image(systemName: image)
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                    .frame(width: 100, height: 100)
+                Text(content).font(.largeTitle)
+            }
         }
     }
 }
 
 struct AudioButtonView_Previews: PreviewProvider {
     static var previews: some View {
-        AudioButtonView(content: "test")
+        ABView(content: "test")
     }
 }
--- TobaTalk/View/ButtonGridView.swift
+++ TobaTalk/View/ButtonGridView.swift
@@ -7,14 +7,14 @@
 
 import SwiftUI
 
-var words = ["Hello", "Toba", "Talk", "Anna", "Tomi", "Owen"]
+var words = [(content: "Hello", image:"photo"), (content: "Toba", image:"photo"), (content: "Talk", image:"photo"), (content: "Anna", image:"photo"), (content: "Tomi", image:"photo"), (content: "Owen", image:"photo")]
 
 struct ButtonGridView: View {
     var body: some View {
         VStack {
             LazyVGrid (columns: [GridItem(), GridItem(), GridItem()]) {
-                ForEach (words[0...words.count-1], id: \.self) { word in
-                    AudioButtonView(content: word)
+                ForEach (words[0...words.count-1], id: \.self.content) { word in
+                    ABView(content: word.content, image: word.image)
                 }
             }
         }
