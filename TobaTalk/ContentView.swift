682bb4e900e5e51e679fd0cad18b698b1258c1f1
Owen Waldron <owaldron@tfs.ca>
2022-01-30 16:01:14 -0500

    Initial Commit

---
+++ TobaTalk/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  TobaTalk
+//
+//  Created by Owen Waldron on 2022-01-30.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ TobaTalk/TobaTalkApp.swift
@@ -0,0 +1,17 @@
+//
+//  TobaTalkApp.swift
+//  TobaTalk
+//
+//  Created by Owen Waldron on 2022-01-30.
+//
+
+import SwiftUI
+
+@main
+struct TobaTalkApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
